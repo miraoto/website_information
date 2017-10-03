f100c3fe292d58f98fc2b315bf1f49953b3e2a27
miraoto <miraoto@gmail.com>
2017-10-03 23:46:52 +0900

    Sort methods

--- lib/website_information/website.rb
+++ lib/website_information/website.rb
@@ -28,16 +28,16 @@ module WebsiteInformation
       sns(doc)
     end
 
+    def feed(doc)
+      @params.feed = doc.css('//link[@rel="alternate"][@type="application/atom+xml"]/@href')[0].to_s
+      @params.feed = doc.css('//link[@rel="alternate"][@type="application/rss+xml"]/@href')[0].to_s if @params.feed.empty?
+    end
+
     def sns(doc)
       # scrape facebook url from page plugin (https://developers.facebook.com/docs/plugins/page-plugin/)
       @params.sns.facebook = doc.css('//div[class$="fb-page"]/@data-href')[0].to_s
       # scrape twitter url from embedded timelines (https://dev.twitter.com/web/embedded-timelines/list)
       @params.sns.twitter = doc.css('//a[class$="twitter-timeline"]/@href')[0].to_s
-    end
-
-    def feed(doc)
-      @params.feed = doc.css('//link[@rel="alternate"][@type="application/atom+xml"]/@href')[0].to_s
-      @params.feed = doc.css('//link[@rel="alternate"][@type="application/rss+xml"]/@href')[0].to_s if @params.feed.empty?
     end
   end
 end
