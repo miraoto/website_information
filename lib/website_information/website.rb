e7589424510314ba28aee9730e1dee977739ab16
miraoto <miraoto@gmail.com>
2017-10-03 23:13:26 +0900

    Add comment

--- lib/website_information/website.rb
+++ lib/website_information/website.rb
@@ -31,6 +31,7 @@ module WebsiteInformation
     def sns(doc)
       # scrape facebook url from page plugin (https://developers.facebook.com/docs/plugins/page-plugin/)
       @params.sns.facebook = doc.css('//div[class$="fb-page"]/@data-href')[0].to_s
+      # scrape twitter url from embedded timelines (https://dev.twitter.com/web/embedded-timelines/list)
       @params.sns.twitter = doc.css('//a[class$="twitter-timeline"]/@href')[0].to_s
     end
 
