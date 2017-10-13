403a97d87e3e90ce0de3330ad75d980c3ad5d141
miraoto <miraoto@gmail.com>
2017-10-13 14:47:19 +0900

    Remove TODO

--- lib/website_information/params/site.rb
+++ lib/website_information/params/site.rb
@@ -28,7 +28,7 @@ module WebsiteInformation
         if @capture.nil?
           options = Selenium::WebDriver::Firefox::Options.new
           options.add_argument('-headless')
-          # TODO if 'https://github.com/SeleniumHQ/selenium/issues/4591' pull-request merged, change above code. 'options.headless!'
+          # if 'https://github.com/SeleniumHQ/selenium/issues/4591' pull-request merged, change above code. 'options.headless!'
           browser = Selenium::WebDriver.for :firefox, options: options
           browser.get @url
           @capture = browser.screenshot_as(:png)
