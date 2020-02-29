dbc36475bc5c78e26dc5c42227997ae5da2c814b
miraoto <miraoto@gmail.com>
2020-02-29 23:57:11 +0900

    Update gems and fix test

--- spec/website_information_spec.rb
+++ spec/website_information_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe WebsiteInformation do
   end
 
   let(:params) { WebsiteInformation::Website.new('https://github.com/miraoto/website_information').scraped_params }
-  let(:params_for_sns_case) { WebsiteInformation::Website.new('https://www.c-notes.jp/').scraped_params }
 
   it "Valid initial information for scraping" do
     expect(params.url).to eq('https://github.com/miraoto/website_information')
@@ -17,7 +16,7 @@ RSpec.describe WebsiteInformation do
   end
 
   it "Valid scraped 'favicon' params" do
-    expect(params.favicon).to eq('https://assets-cdn.github.com/favicon.ico')
+    expect(params.favicon).to eq('https://github.githubassets.com/favicon.ico')
 
     params_for_favicon_path = WebsiteInformation::Website.new('https://rubygems.org').scraped_params
     expect(params_for_favicon_path.favicon).to eq('https://rubygems.org/favicon.ico')
@@ -39,21 +38,15 @@ RSpec.describe WebsiteInformation do
 
 
   it "Valid scraped 'meta' params" do
-    expect(params.meta.description).to eq('website_information - scrape website information that title, meta, page capture, description, ogp and so on.')
+    expect(params.meta.description).to eq('scrape website information that title, meta, page capture, description, ogp and so on. - miraoto/website_informationscrape website information that title, meta, page capture, description, ogp and so on. - miraoto/website_information')
   end
 
   it "Valid scraped 'og' params" do
     expect(params.og.site_name).to eq('GitHub')
-    expect(params.og.description).to eq('website_information - scrape website information that title, meta, page capture, description, ogp and so on.')
+    expect(params.og.description).to eq('scrape website information that title, meta, page capture, description, ogp and so on. - miraoto/website_information')
     expect(params.og.title).to eq('miraoto/website_information')
     expect(params.og.url).to eq('https://github.com/miraoto/website_information')
     expect(params.og.image).to eq('https://avatars3.githubusercontent.com/u/1483607?s=400&v=4')
     expect(params.og.type).to eq('object')
   end
-
-  it "Valid scraped 'sns' params" do
-    expect(params_for_sns_case.sns.facebook).to eq('https://www.facebook.com/chiryonote')
-    expect(params_for_sns_case.sns.twitter).to eq('https://twitter.com/chiryonote')
-  end
-
 end
--- website_information.gemspec
+++ website_information.gemspec
@@ -24,7 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "nokogiri"
   spec.add_dependency "selenium-webdriver"
   spec.add_development_dependency "bundler", "~> 1.15"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rake", ">= 12.3.3"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "pry-byebug"
 end
